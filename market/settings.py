c8702bfe378950fc7b5be3c4b8e3c0f372af2190
Andy Nguyen <andynguyen11@gmail.com>
2016-03-24 06:49:19 -0500

    Upgrade django storages

--- market/settings.py
+++ market/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django.contrib.flatpages',
+    'collectfast',
     'password_reset',
     'require',
     'rest_framework',
