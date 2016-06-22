bd9ed4068b4f8ce43c89dd86402502143fabdf71
Andy Nguyen <andynguyen11@gmail.com>
2016-06-22 09:22:15 -0500

    Start react implementation of company creation

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,13 +1,17 @@
+import tagulous
+
 from django.conf.urls import patterns, url
 
 from api.account import ProfileDetail
 from api.payments import BillingView
 from api.company import CompanyListCreate, CompanyDetail
 from api.review import ReviewListCreate
+from business.models import Company
 
 urlpatterns = [
     url(r'billing/$', view=BillingView.as_view()),
     url(r'^company/$', view=CompanyListCreate.as_view(), name='company'),
+    url(r'^company/category/$', tagulous.views.autocomplete, {'tag_model': Company}, name='company-category', ),
     url(r'^company/(?P<pk>[0-9]+)/$', view=CompanyDetail.as_view(), name='company-detail'),
     url(r'^profile/(?P<pk>[0-9]+)/$', view=ProfileDetail.as_view(), name='profile-detail'),
     url(r'^review/$', view=ReviewListCreate.as_view(), name='review'),
