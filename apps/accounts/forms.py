1ddaf53fda7b4832e7e9e4fec32d25c2b0d10c90
Alfredo Yebra Jr <fred.yebra@gmail.com>
2016-06-28 15:39:44 -0500

    Add signup through email and password. (#25)

--- apps/accounts/forms.py
+++ apps/accounts/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Field, Fieldset, Submit
+
 from accounts.models import Profile
 
 
@@ -22,3 +25,36 @@ class ProfileForm(forms.ModelForm):
         model = Profile
         exclude = ('notes', 'signup_code')
         fields = ('first_name', 'last_name', 'location', 'capacity', 'photo', 'biography', )
+
+
+class LoginForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = Profile
+        fields = ('email', 'password')
+
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.form_show_labels = False
+        self.helper.attrs = {
+            'id': 'login-form',
+            'class': 'login-form',
+            'method': 'post'
+        }
+        self.helper.layout = Layout(
+            Fieldset(
+                '',
+                Field('email', placeholder='Email'),
+                Field('password', placeholder='Password'),
+                Submit('submit', 'Sign Up', css_class='btn btn-block'),
+            )
+        )
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        email_query = Profile.objects.filter(email=email)
+        if email_query.exists():
+            raise forms.ValidationError('This email has already been registered')
+        return email
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render_to_response, redirect, render
-from django.contrib.auth import logout as auth_logout
+from django.contrib.auth import logout as auth_logout, authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.template.context import RequestContext
 from django.utils.datastructures import MultiValueDictKeyError
 
 from postman.models import Message
-from accounts.forms import ProfileForm, DeveloperOnboardForm, ManagerOnboardForm
+from accounts.forms import ProfileForm, LoginForm, DeveloperOnboardForm, ManagerOnboardForm
 from accounts.models import Profile
 from business.models import Project, Job, PROJECT_TYPES
 
@@ -19,8 +19,20 @@ def error500(request):
     return render(request, '500.html')
 
 
-def login(request):
-    return render_to_response('login.html', context=RequestContext(request))
+def user_login(request):
+    form = LoginForm()
+    if request.method == 'POST':
+        form = LoginForm(request.POST or None)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+            user.set_password(password)
+            user.save()
+            account = authenticate(username=user.username, password=password)
+            login(request, account)
+            return redirect('confirm-profile')
+    return render(request, 'login.html', {'form': form})
 
 
 def home(request):
@@ -99,7 +111,7 @@ def edit_profile(request):
 @login_required
 def dashboard(request):
     user = Profile.objects.get(id=request.user.id)
-    social = user.social_auth.get(provider='linkedin-oauth2')
+    social = user.social_auth.filter(provider='linkedin-oauth2')
     notifications = user.notifications.unread()
     messages = Message.objects.inbox(request.user, {'is_new': True, })[:5]
     return render_to_response('dashboard.html', {'user': user, 'social': social, 'notifications': notifications, 'messages': messages, }, context_instance=RequestContext(request))
