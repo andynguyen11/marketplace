08f7e0571567e685950e0df57279f4389c83c115
Andy Nguyen <andynguyen11@gmail.com>
2016-08-24 18:02:07 -0500

    Static files madness

--- market/settings.py
+++ market/settings.py
@@ -220,10 +220,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-PROJECT_ROOT = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'assets'),
+    os.path.join(BASE_DIR, "static"),
 )
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 STATICFILES_LOCATION = 'static'
 STATICFILES_STORAGE = 'market.custom_storages.StaticStorage'
 STATIC_URL = "https://%s/%s/" % ('devquity.s3.amazonaws.com', STATICFILES_LOCATION)
