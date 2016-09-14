52c8c3fffa5332d65f54533d158fb9cb21f6e5a9
Andy Nguyen <andynguyen11@gmail.com>
2016-09-14 02:03:43 -0500

    Add unread message email task

--- apps/generics/tasks.py
+++ apps/generics/tasks.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 from celery import shared_task
 
 from accounts.models import Profile
+from postman.models import Message
 from generics.utils import send_mail
 
 
@@ -14,3 +15,12 @@ def account_confirmation(user_id, role=None):
         'fname': user.first_name,
         'email': user.email
     })
+
+
+@shared_task
+def new_message_notification(user_id, thread_id):
+    unread_messages = Message.objects.filter(
+        recipient = user_id,
+        thread=thread_id,
+        read_at__isnull=True
+    ).order_by('-read_at')
