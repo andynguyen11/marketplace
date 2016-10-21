126c745bbe62cc52ebce16f2e914d150f48c5f7d
Andy Nguyen <andynguyen11@gmail.com>
2016-10-21 12:11:27 -0500

    Update cors middleware

--- market/settings.py
+++ market/settings.py
@@ -80,7 +80,7 @@ INSTALLED_APPS = (
 
 MIDDLEWARE_CLASSES = (
     'accounts.middleware.CheckJWT',
-    'corsheaders.middleware.CorsMiddleware',
+    #'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
