dc5df1458288d19d2ea3608a30146a284b116c62
Michael Joseph Rosenthal <rosenthalm93@gmail.com>
2016-11-01 13:55:52 -0500

    check that request is a django driven request before attempting to issue token (#380)

--- apps/accounts/middleware.py
+++ apps/accounts/middleware.py
@@ -5,6 +5,6 @@ from apps.api.utils import set_jwt_token
 class CheckJWT(CorsMiddleware):
     # Check if client IP is allowed
     def process_response(self, request, response):
-        #if not request.COOKIES.get('loom_token', None) and request.user.is_authenticated():
-        #    response = set_jwt_token(response, request.user)
+        if not request.COOKIES.get('loom_token', None) and hasattr(request, 'user') and request.user.is_authenticated():
+            response = set_jwt_token(response, request.user)
         return super(CheckJWT, self).process_response(request, response)
