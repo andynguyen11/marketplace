74bcded4c1c64790685a6a4e505cb8b5372ef61b
Michael Joseph Rosenthal <rosenthalm93@gmail.com>
2016-11-04 12:24:50 -0500

    Promo backend and frontend rework (#378)
    * promos in admin, broke out stripe helpers, viewset checkpoint
    
    * gutted CreditCardView
    
    * Promo backend with:
    * percentage and cash values handled dynamically by `Promo.apply_to`
    * single use and expire checks
    * admins can create at `/admin/payment/promo/`
    * stripe charging consolidated in `Order.pay`
    * correct HTTP codes for promo checking
    
    * added value to is_valid endpoint
    
    * parsing value off
    
    * don't go under zero, make frontend compatible

--- apps/payment/helpers.py
+++ apps/payment/helpers.py
@@ -1,14 +1,47 @@
-from payment.models import Promo
+import stripe
+from django.conf import settings
 
+class Bunch:
+    " generic object from kwargs"
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
 
-def promo_check(code, customer):
-    try:
-        code = code.upper()
-        promo = Promo.objects.get(code=code)
-        if promo.single_use and promo.used:
-            return None
-        if customer in promo.customers.all():
-            return None
-        return promo
-    except Promo.DoesNotExist:
-        return None
+stripe.api_key = settings.STRIPE_KEY
+
+def connect_customer(user, stripe_token):
+    " create a stripe customer with a token from user "
+    stripe_customer = stripe.Customer.create(
+        source=stripe_token,
+        email=user.email,
+        description="{0}, {1} - {2}".format(
+            user.last_name,
+            user.first_name,
+            user.company
+        )
+    )
+    user.stripe = stripe_customer.id
+    user.save()
+    return stripe_customer
+
+def add_source(user, stripe_token):
+    " add a payment source to an existing customer via a token "
+    stripe_customer = stripe.Customer.retrieve(user.stripe)
+    return stripe_customer.sources.create(source=stripe_token)
+
+def get_source(user, source_id):
+    " get a specific stripe payment source's details "
+    stripe_customer = stripe.Customer.retrieve(user.stripe)
+    return stripe_customer.sources.retrieve(source_id)
+
+def charge_source(source, customer, amount, description):
+    " thin wrapper around stripe.Charge.create "
+    stripe.Charge.create(
+        currency='usd',
+        amount=int(amount * 100),
+        source=source,
+        customer=customer,
+        description=description
+    )
+
+stripe_helpers = Bunch(get_source=get_source, add_source=add_source, charge_source=charge_source, connect_customer=connect_customer)
+
--- apps/payment/models.py
+++ apps/payment/models.py
@@ -1,5 +1,8 @@
+from datetime import datetime, date
+from decimal import Decimal
 from django.db import models
 from django.conf import settings
+from payment.helpers import stripe_helpers 
 
 
 class Promo(models.Model):
@@ -11,8 +14,45 @@ class Promo(models.Model):
     used = models.BooleanField(default=False)
     customers = models.ManyToManyField('accounts.Profile', blank=True)
 
+    @property
+    def value_off(self):
+        return str(self.percent_off) + '%' if self.percent_off else '$' + str(self.dollars_off)
+
     def __str__(self):
         return self.code
+
+    def is_valid_for(self, user):
+        return (not user in self.customers.all()) and not (self.single_use and self.used)
+
+    def not_expired(self):
+        return self.expire_date >= date.today()
+
+    def is_valid(self, user):
+        return self.is_valid_for(user) and self.not_expired()
+
+    def mark_used_by(self, user):
+        if self.is_valid_for(user):
+            if self.single_use:
+                self.used = True
+            self.customers.add(user)
+            self.save()
+            return True
+        else:
+            return False
+
+    def apply_to(self, amount):
+        if self.dollars_off:
+            return amount - self.dollars_off
+        if self.percent_off:
+            return round(amount - (amount * Decimal(self.percent_off * 0.01)), 2)
+        return amount if amount > 0 else 0.00
+
+
+def get_promo(code):
+    try:
+        return Promo.objects.get(code=code)
+    except Promo.DoesNotExist:
+        return None
 
 
 class Order(models.Model):
@@ -20,7 +60,6 @@ class Order(models.Model):
     date_charged = models.DateTimeField(blank=True, null=True)
     job = models.OneToOneField('business.Job')
     promo = models.ForeignKey(Promo, blank=True, null=True)
-    date_created = models.DateTimeField(auto_now=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     status = models.CharField(max_length=100, default='pending')
 
@@ -31,3 +70,36 @@ class Order(models.Model):
         if not self.price:
             self.price = self.job.hours * settings.LOOM_FEE
         super(Order, self).save(*args, **kwargs)
+
+    @property
+    def payer(self):
+        return self.job.project.project_manager
+
+    @property
+    def final_price(self):
+        return self.promo.apply_to(self.price) if self.promo else self.price
+
+    def add_promo(self, code): # TODO: Should an incorrect promo fail silently like this?
+        if code:
+            promo = get_promo(code)
+            if promo and promo.is_valid(self.payer):
+                self.promo = promo
+            else:
+                return False
+
+    def can_pay(self, user):
+        return user == self.payer
+
+    def pay(self, customer, card):
+        stripe_helpers.charge_source(
+            amount=self.final_price,
+            source=card,
+            customer=customer,
+            description='Loom fee for "{0}"'.format(self.job.project.title)
+        )
+        if self.promo:
+            self.promo.mark_used_by(self.payer)
+        self.date_charged = datetime.now()
+        self.status = 'paid'
+        self.save()
+
