2c1c686e522594425e55039982c4fd159726f477
Andy Nguyen <andynguyen11@gmail.com>
2016-11-10 16:01:19 -0600

    Add regex to message filters

--- assets/js/utils/formHelpers.js
+++ assets/js/utils/formHelpers.js
@@ -1,17 +1,12 @@
 import parseFormat from 'moment-parseformat';
 
 const filters = {
-  'pii': {
-    'error': 'Words',
-    'list': [
-      /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i
-    ]
-  },
-  'badWords': {
-    'error': 'Bad Words',
-    'list': [
-      'fuck',
-      'shit'
+  "pii": {
+    "error": "Words",
+    "list": [
+      /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i,
+      /^[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/i,
+      /^\+?\d{2}[- ]?\d{3}[- ]?\d{5}$/
     ]
   }
 }
@@ -63,7 +58,10 @@ const FormHelpers = {
   filterInput: function(string) {
     for (var filter in filters) {
       for (var i = 0; i < filters[filter]['list'].length; i++) {
-        if (string.toLowerCase().search(filters[filter]['list'][i]) != -1) {
+        const searchString = string.toLowerCase().replace(/\s+/g, '')
+        console.log(searchString);
+        console.log(searchString.search(filters[filter]['list'][i]));
+        if (searchString.search(filters[filter]['list'][i]) != -1) {
           return filters[filter]['error'];
         }
       }
