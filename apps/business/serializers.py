840aa7728cd20f97855d5907c1ff604f85dc5943
Andy Nguyen <andynguyen11@users.noreply.github.com>
2017-03-31 22:50:47 -0500

    Release v0.4.0 bug fixes (#618)
    * Proposal polish (#616)
    
    * Copy changes to support proposals (#613)
    
    * Proposal polish, international postal code api support (#615)
    
    * Proposal polish, international postal code api support
    
    * Add new SPA files
    
    * Fix spa template
    
    * Project page updates for proposals, fix repeating subject but

--- apps/business/serializers.py
+++ apps/business/serializers.py
@@ -54,6 +54,7 @@ class ProjectSerializer(JSONFormSerializer, ParentModelSerializer):
     questions = serializers.SerializerMethodField()
     proposal = serializers.SerializerMethodField()
     message = serializers.SerializerMethodField()
+    skill_names = serializers.SerializerMethodField()
 
     class Meta:
         model = Project
@@ -94,6 +95,9 @@ class ProjectSerializer(JSONFormSerializer, ParentModelSerializer):
     def get_questions(self, obj):
         questions = Question.objects.filter(project=obj, active=True).order_by('ordering')
         return QuestionSerializer(questions, many=True).data
+
+    def get_skill_names(self, obj):
+        return [skill.name for skill in obj.skills.all()]
 
 
 class ProjectSearchSerializer(HaystackSerializerMixin, ProjectSerializer):
