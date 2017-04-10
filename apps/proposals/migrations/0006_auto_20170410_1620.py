6c0d0174e16b9d3a6929b32381cdfeda18c243c0
Andy Nguyen <andynguyen11@gmail.com>
2017-04-10 14:54:45 -0500

    Fix proposal migrations

--- apps/proposals/migrations/0006_auto_20170410_1620.py
+++ apps/proposals/migrations/0006_auto_20170410_1620.py
@@ -4,6 +4,13 @@ from __future__ import unicode_literals
 
 from django.db import migrations, models
 
+def set_cash_equity_values(apps, schema_editor):
+    Proposal = apps.get_model('proposals', 'Proposal')
+    for proposal in Proposal.objects.all():
+        proposal.equity_flag = True if proposal.equity else False
+        proposal.cash_flag = True if proposal.cash else False
+        proposal.save()
+
 
 class Migration(migrations.Migration):
 
@@ -12,18 +19,39 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.RunSQL('SET CONSTRAINTS ALL IMMEDIATE', reverse_sql=migrations.RunSQL.noop),
+        migrations.AddField(
+            model_name='proposal',
+            name='cash_flag',
+            field=models.BooleanField(default=False),
+        ),
+        migrations.AddField(
+            model_name='proposal',
+            name='equity_flag',
+            field=models.BooleanField(default=False),
+        ),
+        migrations.RunPython(set_cash_equity_values),
+        migrations.RemoveField(
+            model_name='proposal',
+            name='cash',
+        ),
+        migrations.RemoveField(
+            model_name='proposal',
+            name='equity',
+        ),
+        migrations.RenameField(
+            model_name='proposal',
+            old_name='cash_flag',
+            new_name='cash',
+        ),
+        migrations.RenameField(
+            model_name='proposal',
+            old_name='equity_flag',
+            new_name='equity',
+        ),
         migrations.AlterModelOptions(
             name='proposal',
             options={'ordering': ('-create_date',)},
         ),
-        migrations.AlterField(
-            model_name='proposal',
-            name='cash',
-            field=models.BooleanField(default=False),
-        ),
-        migrations.AlterField(
-            model_name='proposal',
-            name='equity',
-            field=models.BooleanField(default=False),
-        ),
+        migrations.RunSQL(migrations.RunSQL.noop, reverse_sql='SET CONSTRAINTS ALL IMMEDIATE'),
     ]
