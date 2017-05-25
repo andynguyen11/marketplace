5dca5dbd27ee958b71c7ff100f0edb07a3f814ec
Andy Nguyen <andynguyen11@users.noreply.github.com>
2017-05-25 13:08:30 -0500

    Admin search on user and project (#677)

--- apps/business/admin.py
+++ apps/business/admin.py
@@ -12,12 +12,12 @@ class CompanyAdmin(admin.ModelAdmin):
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('title', 'company', 'project_manager', )
+    search_fields = ('title', 'company__name', 'project_manager__email', 'project_manager__first_name', 'project_manager__last_name',  )
 
 
 @admin.register(Job)
 class JobAdmin(admin.ModelAdmin):
     list_display = ('project', 'contractor', 'date_created', 'status', )
-    inlines = [AttachmentInline]
 
 
 @admin.register(Document)
