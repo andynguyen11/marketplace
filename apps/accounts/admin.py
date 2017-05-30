fa743d8ae753261813138176373593345b8d2ba4
Andy Nguyen <andynguyen11@users.noreply.github.com>
2017-05-30 12:34:59 -0500

    Release v0.4.6 (#679)
    * Adjust celery daemon
    
    * Admin search on user and project (#677)
    
    * Freelancer discovery behind admin decorator

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -3,5 +3,7 @@ from accounts.models import Profile
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('last_name', 'first_name', 'email')
+    list_display = ('email', 'last_name', 'first_name', )
+    list_display_links = ('email', 'first_name', 'last_name', )
+    search_fields = ('email', 'first_name', 'last_name', )
 
