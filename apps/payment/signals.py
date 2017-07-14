fd4e6c408b93ededc0ad66c43e4b83061fe14547
Andy Nguyen <andynguyen11@users.noreply.github.com>
2017-07-14 10:12:33 -0500

    LM-1051 & LM-1033 (#735)
    * Handle one off token payment, add payment_enabled to JWT
    
    * Payment notifications

--- apps/payment/signals.py
+++ apps/payment/signals.py
@@ -14,14 +14,29 @@ def field_changed(instance, field, id_field='id'):
 @receiver(post_save, sender='payment.Invoice')
 def invoice_notifications(sender, instance, created, **kwargs):
     old_instance = Invoice.objects.get(id=instance.id)
+
+    # Invoice sent
     if created and instance.status == 'sent':
-        invoice_notification_email('invoice-received', instance.sender_name, instance.recipient_email, instance.id)
+        return invoice_notification_email('invoice-received', instance.sender_name, instance.recipient_email, instance.id)
 
+    # Invoice sent
     if old_instance.status == 'draft' and instance.status == 'sent':
-        invoice_notification_email('invoice-received', instance.sender_name, instance.recipient_email, instance.id)
+        return invoice_notification_email('invoice-received', instance.sender_name, instance.recipient_email, instance.id)
 
+    # Invoice viewed
     if not old_instance.viewed and instance.viewed:
-        invoice_notification_email('invoice-read', instance.recipient_name, instance.sender_email, instance.id)
+        return invoice_notification_email('invoice-read', instance.recipient_name, instance.sender_email, instance.id)
 
+    # Invoice updated
     if old_instance.status == 'sent' and instance.status == 'sent':
-        invoice_notification_email('invoice-updated', instance.sender_name, instance.recipient_email, instance.id)
+        return invoice_notification_email('invoice-updated', instance.sender_name, instance.recipient_email, instance.id)
+
+    # Invoice paid
+    if old_instance.status == 'sent' and instance.status == 'paid':
+        invoice_notification_email('payment-sent', instance.sender_name, instance.recipient_email, instance.id)
+        paid_invoices =  Invoice.objects.filter(sender=instance.sender, status='paid')
+        number_of_invoices = len(paid_invoices)
+        if number_of_invoices == 1:
+            return invoice_notification_email('first-payment-received', instance.recipient_name, instance.sender_email, instance.id)
+        else if number_of_invoices > 1:
+            return invoice_notification_email('payment-received', instance.recipient_name, instance.sender_email, instance.id)
