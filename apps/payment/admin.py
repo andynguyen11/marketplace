92e72fadbbbfd7bc64a82dbe584e65924134898a
Andy Nguyen <andynguyen11@users.noreply.github.com>
2017-08-24 12:50:47 -0500

    Messages admin (#782)

--- apps/payment/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from payment.models import Promo
-
-@admin.register(Promo)
-class PromoAdmin(admin.ModelAdmin):
-    list_display = ('code', 'expire_date', 'value_off', )
-
-
--- apps/postman/admin.py
+++ apps/postman/admin.py
@@ -1,5 +1,7 @@
+from django.contrib.admin.utils import unquote
 from django.contrib import admin
 from django.db.models import F
+from django.template.response import TemplateResponse
 
 from postman.models import Message
 
@@ -10,5 +12,24 @@ class MessageAdmin(admin.ModelAdmin):
     def get_queryset(self, request):
         qs = super(MessageAdmin, self).get_queryset(request)
         return qs.filter(id=F('thread'))
+
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        to_field = request.GET.get('_to_field')
+        model = self.model
+        opts = model._meta
+        obj = self.get_object(request, unquote(object_id), to_field)
+        messages = Message.objects.filter(thread=obj).exclude(body__exact='')
+
+        context = dict(
+            self.admin_site.each_context(request),
+            title='Messages',
+            opts=opts,
+            object_id=object_id,
+            original=obj,
+            messages=messages
+        )
+
+        return TemplateResponse(request, "admin/%s/%s/change_form.html" % (opts.app_label, opts.model_name), context)
+
 
 admin.site.register(Message, MessageAdmin)
