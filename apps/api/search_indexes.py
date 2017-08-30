a8010b43854826880dc88bd856882c61b8607617
Andy Nguyen <andynguyen11@gmail.com>
2017-08-30 14:17:25 -0500

    Add email_notifications to user index

--- apps/api/search_indexes.py
+++ apps/api/search_indexes.py
@@ -75,4 +75,4 @@ class UserIndex(indexes.ModelSearchIndex, indexes.Indexable):
 
     class Meta:
         model = Profile
-        fields = ("id", "first_name", "last_name", "email", "location", "photo", "roles", "skills" )
+        fields = ("id", "first_name", "last_name", "email", "location", "photo", "roles", "skills", "email_notifications", )
