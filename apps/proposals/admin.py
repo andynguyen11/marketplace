4d972c16bca88353dc12beb545e716805bdc916e
Andy Nguyen <andynguyen11@gmail.com>
2017-08-31 15:14:15 -0500

    Make proposal admin more efficient

--- apps/proposals/admin.py
+++ apps/proposals/admin.py
@@ -6,3 +6,4 @@ class ProposalAdmin(admin.ModelAdmin):
     list_display = ('id', 'create_date', 'submitter', 'project', )
     list_display_links = ('id', )
     search_fields = ( 'submitter', 'project', )
+    exclude = ('message', )
